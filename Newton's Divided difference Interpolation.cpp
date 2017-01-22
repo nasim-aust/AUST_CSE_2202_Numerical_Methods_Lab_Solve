20672acf0fb78610c0b7be79578914b81088452c
MD Abdullah Al Nasim <nasim5153@gmail.com>
2017-01-22 20:45:32 +0600

    Update Newton's Divided difference Interpolation.cpp

--- Newton's Divided difference Interpolation.cpp
+++ Newton's Divided difference Interpolation.cpp
@@ -1,49 +1,39 @@
-#include <bits/stdc++.h>
-#define Max 100
- 
+#include<bits/stdc++.h>
+#define size 101
 using namespace std;
- 
+
 int main()
 {
-    double x[Max],fx[Max],sum = 0.0 , xp,lf;
-    double a[Max],div[Max][Max];
-    int n;
-    printf("Enter the number of set : ");
-    scanf("%d",&n);
-    printf("Enter Data point and the function value of that point : \n\n");
-    for(int i = 1 ; i <= n ;i++)
+    int n,f,f1=1,f2=0,j=1;
+    float x[size],y[size],p[size],value;
+    cout <<"Enter the number of elements: ";
+    cin >> n;
+    for(int i=1; i<=n; i++)
     {
         printf("x[%d] = ",i);
-        scanf("%lf",&x[i]);
-        printf("fx[%d] = ",i);
-        scanf("%lf",&fx[i]);
+        cin >> x[i];
+        printf("y[%d] = ",i);
+        cin >> y[i];
     }
-    printf("Enter the value at which interpolation is required : ");
-    scanf("%lf",&xp);
-    for(int i = 1 ; i <= n ;i++)
-        div[i][1] = fx[i];
-    ///make divided table
-    for(int j = 2 ; j <= n ; j++)
+    f=y[1];
+    cout <<"Enter the value at which interpolation is required : ";
+    cin >> value;
+
+    do
     {
-        for(int i = 1 ; i <= n-j+1 ; i++)
+        for (int i=1;i<=n-1;i++)
         {
-            div[i][j] = (div[i+1][j-1]-div[i][j-1])/(x[i+j-1]-x[i]);
+            p[i] = ((y[i+1]-y[i])/(x[i+j]-x[i]));
+            y[i]=p[i];
         }
-    }
-    for(int i = 1 ; i <= n ; i++)
-        a[i] = div[1][i];
-        sum = a[1];
-    for(int i = 2 ; i <= n ; i++)
-    {
-        lf = 1.0;
-        for(int j = 1 ; j <= i-1 ; j++)
-        {
-            lf *= (xp - x[j]);
-        }
-        sum += lf*a[i];
-    }
-    printf("\n\nNEWTON INTERPOLATION \n\n");
-    printf("Interpolated value \n");
-    printf("at x = %lf is %lf \n",xp,sum);
+        f1=1;
+        for(int i=1;i<=j;i++) f1*=(value-x[i]);
+        f2+=(y[1]*f1);
+        n--;
+        j++;
+    }while(n!=1);
+    f+=f2;
+    cout <<"The answer is :" << f ;
+
     return 0;
 }
